51376a233e8d95cb8fc51b85334e3270620c95cc
Shashank <shashank77ishant@gmail.com>
2017-10-28 22:24:43 -0500

    changed settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -125,9 +125,9 @@ import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-#STATICFILES_DIRS = [
- #   os.path.join(BASE_DIR, "static"),
-#]
+TATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join((BASE_DIR), "static")
